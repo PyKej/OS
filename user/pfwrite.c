b2e9f17d94d6fef5bdff56728b54cae9511272ef
Patrik Kester <patrik.pk13@gmail.com>
2023-10-06 07:34:57 +0200

    final-zap-pfwrite

--- user/pfwrite.c
+++ user/pfwrite.c
@@ -5,15 +5,31 @@
 #include "kernel/fcntl.h"
 
 int
-write_string_to_file(const char file, charmessage) {
-  int fd = -1; // VHODNE UPRAVTE
+write_string_to_file(const char *file, const void *charmessage) {
+  int fd = open(file, O_CREATE | O_WRONLY);
+
+  
 
   if(fd < 0){
-     fprintf(1, "failed to open %s\n", file);
+     fprintf(2, "failed to open %s\n", file);
      exit(1);
   }
 
-  // TU DOPLŇTE KÓD
+  // I want to add newline to the end of the message but it doesn't work
+
+  // if((*(charmessage + strlen(charmessage) - 1) ) != '\n'){
+  //   charmessage = strcat(charmessage, "\n");
+  // }
+
+
+  int n = write(fd, charmessage, strlen(charmessage));
+
+  if(n == -1){
+    fprintf(2, "failed to write to  %s\n", file);
+    exit(1);
+  }
+
+
 
   exit(0);
 }
@@ -21,19 +37,22 @@ write_string_to_file(const char file, charmessage) {
 int
 main(int argc, char *argv[])
 {
-  int i;
 
   if(argc != 4) {
     fprintf(2, "Invalid usage! Usage: fpwriter <file1> <file2> <message>");
     exit(1);
   }
 
-  inr pid = -1; // VHODNE UPRAVTE
+
+  int pid = fork(); // VHODNE UPRAVTE
 
   if(pid > 0){
-     // TU DOPLŇTE KÓD
+     // parent
+    write_string_to_file(argv[1], argv[3]);
+
    } else if(pid == 0){
-     // TU DOPLŇTE KÓD
+     // child
+     write_string_to_file(argv[2], argv[3]);
    } else {
      fprintf(1, "fork failed\n");
    }
