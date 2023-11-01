eccc180c21a03c168ec85df3c0c8482a5c06b183
Patrik Kester <patrik.pk13@gmail.com>
2023-11-01 22:00:43 +0100

    pte printout - Vypísanie tabuľky stránok

--- kernel/exec.c
+++ kernel/exec.c
@@ -128,6 +128,10 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
+  //  aby sa tabulka vypísala len pre prvý proces
+  if(p->pid==1) 
+    vmprint(p->pagetable);
+
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
